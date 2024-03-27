5d8744af9ef9229ad80c447fed72584a24ef5663
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 16:22:56 -0400

    adicionado questao 2

---
+++ questao2.js
@@ -0,0 +1,10 @@
+/*
+Questão 2: Como você pode usar uma declaração if para verificar se uma 
+string está vazia em JavaScript?
+*/
+
+var espaço = "";
+
+if (espaço === "") {
+    console.log("A string está vazia")
+}
