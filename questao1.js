6c3fc1764b94774faa174e2fd7e3fbd726acf994
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 16:22:15 -0400

    adicionado questao 1

---
+++ questao1.js
@@ -0,0 +1,9 @@
+/*
+Questão 1: Como você pode usar uma declaração if para verificar se uma 
+variável é maior que 10 em JavaScript?
+*/
+
+let numero = 12
+if (numero >= 10) {
+    console.log("É MAIOR SEU BURRO");
+}
