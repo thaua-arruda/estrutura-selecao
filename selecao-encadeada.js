6228418f3e5f5310cbbd52c26ac479cb8d6112fe
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 16:00:15 -0400

    adicionado exemplo de estrutura de selecao encadeada

---
+++ selecao-encadeada.js
@@ -0,0 +1,9 @@
+let dia = "Sábado";
+
+if (dia === "Sábado" ) {
+    console.log("Parabéns, você merece descansar.")
+} else if (dia === "domingo") {
+    console.log("Dia de passear com a família!");
+} else {
+    console.log("Levanta e vai estudar!");
+}
