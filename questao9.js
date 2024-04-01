72c27a7db77362a6b857a0e00e1b5c88d62f8380
thaua-arruda <thauafarruda@gmail.com>
2024-04-01 16:54:40 -0400

    add questao9.js

--- questao9.js
+++ questao9.js
@@ -8,12 +8,10 @@ let lado1 = 7;
 let lado2 = 2;
 let lado3 = 3;
  
- function classificarTriangulo (lado1, lado2, lado3,) {
-    if( lado1 === lado2 && lado2 === lado3) {
+if( lado1 === lado2 && lado2 === lado3) {
         console.log("Equilátero")
 } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
     console.log("Isósceles");
 } else {
     console.log("Escaleno")
-} 
 }
